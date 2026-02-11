cef551ff473ebc65112c7c315a8eac821db594a2
Sebastián RS <srodriguezsu@unal.edu.co>
2026-02-11 18:16:09 -0500

    refactor: streamline query selectors in areAsignaturasAvailable function for consistency

--- src/ui/historyUI.js
+++ src/ui/historyUI.js
@@ -11,17 +11,35 @@ export function areAsignaturasAvailable() {
     for (const asignatura of asignaturas) {
 
 
-        let nombreAsignatura = asignatura.querySelector("td.af_column_data-cell.ex-asig-des")?.textContent.trim();
+        const nombreAsignatura = asignatura
+            .querySelector(
+                "td.af_column_data-cell.ex-asig-des, td.af_column_banded-data-cell.ex-asig-des"
+            )?.textContent.trim();
 
-        const creditos = asignatura.querySelector("td.af_column_data-cell.ex-asig-cre.text-right")?.textContent.trim();
+            
+        const creditos = asignatura
+            .querySelector(
+                "td.af_column_data-cell.ex-asig-cre.text-right, td.af_column_banded-data-cell.ex-asig-cre.text-right"
+            )?.textContent.trim();
 
-        const componente = asignatura.querySelector("td.af_column_data-cell.ex-asig-tip")?.textContent.trim();
+        const componente = asignatura
+            .querySelector(
+                "td.af_column_data-cell.ex-asig-tip, td.af_column_banded-data-cell.ex-asig-tip"
+            )?.textContent.trim();
         
-        const semestre = asignatura.querySelector("td.af_column_data-cell.ex-asig-conv")?.textContent.trim();
+        const semestre = asignatura
+            .querySelector(
+                "td.af_column_data-cell.ex-asig-conv, td.af_column_banded-data-cell.ex-asig-conv"
+            )?.textContent.trim();
 
-        const calificacion = asignatura.querySelector("td.af_column_data-cell.ex-asig-cal.text-center")?.textContent.trim();
+        const calificacion_estado = asignatura
+            .querySelector(
+                "td.af_column_data-cell.ex-asig-cal.text-center, td.af_column_banded-data-cell.ex-asig-cal.text-center"
+            )?.textContent.trim();
 
-        console.log(`Asignatura: ${nombreAsignatura}, Créditos: ${creditos}, Componente: ${componente}, Semestre: ${semestre}, Calificación: ${calificacion}`);
+        console.log(`Asignatura: ${nombreAsignatura}, Créditos: ${creditos}, Componente: ${componente}, Semestre: ${semestre}, Calificación: ${calificacion_estado}`);    
+
+        
 
     }
         
